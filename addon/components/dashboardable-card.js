d7d2d9ddfdb9cee0725b8f13682c2c2e7b2cffa6
Gabriel Campes <gabrieldecampes@gmail.com>
2015-12-12 21:58:21 -0200

    Remove useless class code from card

--- addon/components/dashboardable-card.js
+++ addon/components/dashboardable-card.js
@@ -2,16 +2,5 @@ import Ember from 'ember';
 import DashboardableMixin from '../mixins/dashboardable';
 
 export default Ember.Component.extend(DashboardableMixin, {
-  classNames: ['dashboardable-card'],
-
-  init(){
-    this._super();
-    setTimeout(() => {
-      if(this.get('classesToAdd')){
-        this.get('classesToAdd').split(' ').forEach((className) => {
-          $(this.get('element')).addClass(className);
-        });
-      }
-    });
-  }
+  classNames: ['dashboardable-card']
 });
